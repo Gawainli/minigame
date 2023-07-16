9ba663bdf4dc1fcf2990f45c3930440e6ac3101a
Gawain <gawainli@outlook.com>
2023-07-16 23:32:35 +0800

    feat:添加测试代码

--- Assets/_GameMain/Code/MiniGameTest.cs
+++ Assets/_GameMain/Code/MiniGameTest.cs
@@ -1,18 +1,41 @@
+using System;
+using Cysharp.Threading.Tasks;
+using MiniGame.Event;
 using UnityEngine;
 using MiniGame.Logger;
+
+public class TestEvent : IEventMessage
+{
+    public string message;
+}
 
 public class MiniGameTest : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-        MLogger.Info("Test Info");
-        MLogger.Warning("Test Warning");
+        LogModule.Info("Test Info");
+        LogModule.Warning("Test Warning");
+        EventModule.AddListener<TestEvent>(OnEventTest);
+
+        await UniTask.Delay(TimeSpan.FromSeconds(1));
+        EventModule.SendEvent(new TestEvent { message = "Test Event" });
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnEventTest(IEventMessage msg)
+    {
+        var testMsg = msg as TestEvent;
+        LogModule.Info(testMsg.message);
+    }
+    
+    private void OnDestroy()
+    {
+        EventModule.RemoveListener<TestEvent>(OnEventTest);
     }
 }
