f1e2774329ce98c642588ec7d41388d9a400afcb
lijun <jun.gawain@gmail.com>
2023-07-17 21:18:44 +0800

    feat:添加scene模块

---
+++ Assets/MiniGame/Scene/SceneModule.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using MiniGame.Module;
+
+namespace MiniGame.Scene
+{
+    public class SceneModule : IModule
+    {
+        public static Stack<UnityEngine.SceneManagement.Scene> SceneStack = new Stack<UnityEngine.SceneManagement.Scene>();
+        
+        
+        public void Initialize(object userData = null)
+        {
+        }
+
+        public void Tick(float deltaTime, float unscaledDeltaTime)
+        {
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        public int Priority { get; set; }
+    }
+}
