f6cc5b06037973a90280f87db32516b5570b3065
lijun <jun.gawain@gmail.com>
2023-07-18 20:52:12 +0800

    feat:添加UI Module空代码

---
+++ Assets/MiniGame/UI/UIModule.cs
@@ -0,0 +1,31 @@
+﻿using MiniGame.Logger;
+using MiniGame.Module;
+using UnityEngine;
+
+namespace MiniGame.UI
+{
+    public class UIModule : IModule
+    {
+        
+        
+        #region IModule
+        public void Initialize(object userData = null)
+        {
+            LogModule.Info("UIModule Initialize");
+            Initialized = true;
+        }
+
+        public void Tick(float deltaTime, float unscaledDeltaTime)
+        {
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        public int Priority { get; set; }
+        public bool Initialized { get; set; }
+
+        #endregion
+    }
+}
