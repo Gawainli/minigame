70bc36642b01c4bfacd298d078c517900e3598d1
lijun <jun.gawain@gmail.com>
2023-07-19 22:15:10 +0800

    feat:ui 窗口代码

--- Assets/MiniGame/UI/UIModule.cs
+++ Assets/MiniGame/UI/UIModule.cs
@@ -1,22 +1,121 @@
-﻿using MiniGame.Logger;
+﻿using System.Collections.Generic;
+using MiniGame.Logger;
 using MiniGame.Module;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MiniGame.UI
 {
+    public class UIModuleCfg
+    {
+        public bool pixelPerfect = false;
+        public bool matchWidthOrHeight = false;
+        public float referenceResolutionX = 1920;
+        public float referenceResolutionY = 1080;
+        
+        public bool ignoreReversedGraphics = false;
+    }
     public class UIModule : IModule
     {
+        internal static GameObject UIRoot;
+        internal static readonly List<UIWindow> WindowStack = new List<UIWindow>();
+
+        private GameObject NewUIRoot(UIModuleCfg cfg)
+        {
+            var root = new GameObject("UIRoot");
+            var canvas = root.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            canvas.pixelPerfect = cfg.pixelPerfect;
+            var scaler = root.AddComponent<CanvasScaler>();
+            scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            scaler.matchWidthOrHeight = cfg.matchWidthOrHeight? 0 : 1;
+            scaler.referenceResolution = new Vector2(cfg.referenceResolutionX, cfg.referenceResolutionY);
+            var raycaster = root.AddComponent<GraphicRaycaster>();
+            raycaster.ignoreReversedGraphics = cfg.ignoreReversedGraphics;
+            return root;
+        }
+        
+        private static void PushWindow(UIWindow window)
+        {
+            if (window == null)
+            {
+                return;
+            }
+
+            var index = -1;
+            for (int i = 0; i < WindowStack.Count; i++)
+            {
+                if (window.WindowLayer < WindowStack[i].WindowLayer)
+                {
+                    index = i + 1;
+                }
+            }
+
+            if (index == -1)
+            {
+                for (int i = 0; i < WindowStack.Count; i++)
+                {
+                    if (window.WindowLayer > WindowStack[i].WindowLayer)
+                    {
+                        index = i + 1;
+                    }
+                }
+            }
+
+            if (index == -1)
+            {
+                index = 0;
+            }
+            
+            WindowStack.Insert(index, window);
+        }
+        
+        private static void PopWindow(UIWindow window)
+        {
+            WindowStack.Remove(window);
+        }
+        
+        private static void ContainsWindow(UIWindow window)
+        {
+            foreach(var ui in WindowStack)
+            {
+                if (ui.WindowName == window.WindowName)
+                {
+                    return;
+                }
+            }
+        }
+        
         
         
         #region IModule
         public void Initialize(object userData = null)
         {
             LogModule.Info("UIModule Initialize");
+            if (userData is UnityEngine.GameObject uiRoot)
+            {
+                UIRoot = uiRoot;
+            }
+            else if(userData is UIModuleCfg cfg)
+            {
+                UIRoot = NewUIRoot(cfg);
+            }
+            else if(userData == null)
+            {
+                UIRoot = NewUIRoot(new UIModuleCfg());
+            }
             Initialized = true;
         }
-
+        
         public void Tick(float deltaTime, float unscaledDeltaTime)
         {
+            if (Initialized)
+            {
+                foreach (var ui in WindowStack)
+                {
+                    ui?.Update(deltaTime);
+                }
+            }
         }
 
         public void Shutdown()
---
+++ Assets/MiniGame/UI/UIWindow.cs
@@ -0,0 +1,214 @@
+﻿using Cysharp.Threading.Tasks;
+using MiniGame.Asset;
+using MiniGame.Logger;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MiniGame.UI
+{
+    public abstract class UIWindow
+    {
+        private System.Object[] _userDatas;
+        private GameObject _uiPanel;
+        private Canvas _uiCanvas;
+        private Canvas[] _uiChildCanvas;
+        private bool _isCreate = false;
+        private GraphicRaycaster _uiGraphicRaycaster;
+        private GraphicRaycaster[] _uiChildGraphicRaycaster;
+        
+        public Transform Transform => _uiPanel.transform;
+        public GameObject UIPanel => _uiPanel;
+        public System.Object[] UserDatas => _userDatas;
+        
+        public string WindowName { get; private set; }
+        public int WindowLayer { get; private set; }
+        public int WindowId { get; private set; }
+        
+        
+        public int Depth
+        {
+            get => _uiCanvas != null ? _uiCanvas.sortingOrder : -999;
+            set
+            {
+                if (_uiCanvas != null)
+                {
+                    if (_uiCanvas.sortingOrder == value)
+                    {
+                        return;
+                    }
+                    _uiCanvas.sortingOrder = value;
+                    int depth = value;
+                    foreach (var child in _uiChildCanvas)
+                    {
+                        if (child != null && child != _uiCanvas)
+                        {
+                            depth++;
+                            child.sortingOrder = depth;
+                        }
+                    }
+                }
+
+                if (_isCreate)
+                {
+                    OnSortDepth(value);
+                }
+            }
+        }
+        
+        public bool Visible
+        {
+            get => _uiPanel != null && _uiPanel.activeSelf;
+            set
+            {
+                if (_uiPanel != null && _uiPanel.activeSelf != value)
+                {
+                    _uiPanel.SetActive(value);
+
+                    if (_isCreate)
+                    {
+                        OnSetVisible(value);
+                    }
+                }
+            }
+        }
+        
+        public bool Interactable
+        {
+            get => _uiGraphicRaycaster && _uiGraphicRaycaster.enabled;
+            set
+            {
+                if (_uiGraphicRaycaster && _uiGraphicRaycaster.enabled != value)
+                {
+                    _uiGraphicRaycaster.enabled = value;
+                    foreach (var rc in _uiChildGraphicRaycaster)
+                    {
+                        rc.enabled = value;
+                    }
+                }
+            }
+        }
+        
+        public void Init(string windowName, int windowId, System.Object[] userDatas)
+        {
+            WindowName = windowName;
+            WindowId = windowId;
+            _userDatas = userDatas;
+        }
+        
+        /// <summary>
+        /// 窗口创建
+        /// </summary>
+        public abstract void OnCreate();
+
+        /// <summary>
+        /// 窗口刷新
+        /// </summary>
+        public abstract void OnRefresh();
+
+        /// <summary>
+        /// 窗口更新
+        /// </summary>
+        public abstract void OnUpdate(float deltaTime);
+
+        /// <summary>
+        /// 窗口销毁
+        /// </summary>
+        public abstract void OnDestroy();
+
+        /// <summary>
+        /// 当触发窗口的层级排序
+        /// </summary>
+        protected virtual void OnSortDepth(int depth) { }
+
+        /// <summary>
+        /// 当因为全屏遮挡触发窗口的显隐
+        /// </summary>
+        protected virtual void OnSetVisible(bool visible) { }
+
+        public async UniTask LoadAsync(string assetPath, System.Object[] userDatas)
+        {
+            _userDatas = userDatas;
+            var panelPrefab = await AssetModule.LoadAssetAsync<GameObject>(assetPath);
+            if (panelPrefab == null)
+            {
+                LogModule.Error("UIWindow Load Error: panelPrefab is null. path: " + assetPath);
+                return;
+            }
+            InstantiatePanel(panelPrefab);
+        }
+        
+        public void LoadSync(string assetPath, System.Object[] userDatas)
+        {
+            _userDatas = userDatas;
+            var panelPrefab = AssetModule.LoadAssetSync<GameObject>(assetPath);
+            if (panelPrefab == null)
+            {
+                LogModule.Error("UIWindow Load Error: panelPrefab is null. path: " + assetPath);
+                return;
+            }
+            InstantiatePanel(panelPrefab);
+        }
+
+        private void InstantiatePanel(GameObject panelPrefab)
+        {
+            _uiPanel = Object.Instantiate(panelPrefab);
+            _uiPanel.transform.localPosition = Vector3.zero;
+            
+            _uiCanvas = _uiPanel.GetComponent<Canvas>();
+            if (_uiCanvas == null)
+            {
+                LogModule.Error("UIWindow Load Error: Canvas is null. name: " + panelPrefab.name);
+            }
+            _uiCanvas.overrideSorting = true;
+            _uiCanvas.sortingOrder = 0;
+            _uiCanvas.sortingLayerName = "UI";
+            
+            _uiGraphicRaycaster = _uiPanel.GetComponent<GraphicRaycaster>();
+            _uiChildCanvas = _uiPanel.GetComponentsInChildren<Canvas>();
+            _uiChildGraphicRaycaster = _uiPanel.GetComponentsInChildren<GraphicRaycaster>(); 
+        }
+        
+        
+        internal void Create()
+        {
+            if (_isCreate)
+            {
+                return;
+            }
+            OnCreate();
+        }
+        
+        internal void Refresh()
+        {
+            if (!_isCreate)
+            {
+                return;
+            }
+            OnRefresh();
+        }
+        
+        internal void Update(float deltaTime)
+        {
+            if (!_isCreate)
+            {
+                return;
+            }
+            OnUpdate(deltaTime);
+        }
+        
+        internal void Destroy()
+        {
+            if (!_isCreate)
+            {
+                return;
+            }
+
+            if (_uiPanel != null)
+            {
+                OnDestroy();
+                Object.Destroy(_uiPanel);
+                _uiPanel = null;
+            }
+        }
+    }
+}
