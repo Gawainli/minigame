92328a7873aba4a62a72579da25603ba95abf18e
lijun <jun.gawain@gmail.com>
2023-08-14 14:58:51 +0800

    feat:添加luban导表测试

--- DesignerData/gen.bat
+++ DesignerData/gen.bat
@@ -5,8 +5,10 @@ set CONF_ROOT=.
 dotnet %LUBAN_DLL% ^
     -t all ^
     -d json ^
+    -c cs-simple-json ^
     --schemaPath %CONF_ROOT%\Defines\__root__.xml ^
     -x inputDataDir=%CONF_ROOT%\Datas  ^
-    -x outputDataDir=output
+    -x outputCodeDir=%WORKSPACE%\Assets\_GameMain\Code\HotUpdate\Data\Gen^
+    -x outputDataDir=%WORKSPACE%\Assets\_GameMain\Data
 
 pause
