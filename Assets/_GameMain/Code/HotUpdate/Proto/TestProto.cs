1687f8db72f9f1cd0049f73996d4ba13ae5fcf56
lijun <jun.gawain@gmail.com>
2023-08-14 18:58:05 +0800

    feat:添加Protobuf

--- Assets/_GameMain/Code/HotUpdate/Proto/TestProto.cs
+++ Assets/_GameMain/Code/HotUpdate/Proto/TestProto.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using MiniGame.Logger;
 using UnityEngine;
 
 public class TestProto : MonoBehaviour
@@ -13,7 +15,17 @@ public class TestProto : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        var person = new Person{ Name = "Tom", Age = 18 };
+        var ms = new MemoryStream();
+        ProtoBuf.Serializer.Serialize(ms, person);
+        var bytes = ms.ToArray();
+        LogModule.Info("bytes length: " + bytes.Length);
+        ms.Close();
         
+        var ms2 = new MemoryStream(bytes);
+        var newPerson = ProtoBuf.Serializer.Deserialize<Person>(ms2);
+        LogModule.Info("newPerson: " + newPerson.Name+ " " + newPerson.Age);
+        ms2.Close();
     }
 
     // Update is called once per frame
