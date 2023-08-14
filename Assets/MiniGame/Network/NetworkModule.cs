9b84945cda03fdf51e491d88b92b5f111f02801c
lijun <jun.gawain@gmail.com>
2023-08-14 23:12:07 +0800

    chore:添加network

---
+++ Assets/MiniGame/Network/NetworkModule.cs
@@ -0,0 +1,25 @@
+﻿using MiniGame.Module;
+
+namespace MiniGame.Network
+{
+    public class NetworkModule : IModule
+    {
+        public void Initialize(object userData = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Tick(float deltaTime, float unscaledDeltaTime)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Shutdown()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Priority { get; set; }
+        public bool Initialized { get; set; }
+    }
+}
